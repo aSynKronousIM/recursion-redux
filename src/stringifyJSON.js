21e1964bbe9836eceb0bcee605e28939acde20c7
Sean Kim <skim2636@gmail.com>
2015-02-02 16:35:48 -0800

    fixed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,51 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function (obj) {
+
+
+  if(Array.isArray(obj)){
+    var resultArray = [];
+    for(var i = 0, length = obj.length; i < length; i ++){
+      resultArray.push(stringifyJSON(obj[i]));
+    }
+    return '['+ resultArray.join(',') +']'; 
+  }
+  
+  if(obj === null) {
+    return ''+null;
+  }
+
+  if (typeof obj === 'object') {
+    var resultArray = [];
+    for (var key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === 'function') {
+        continue;
+      }
+      resultArray.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+    }
+    return "{" + resultArray.join(',') + "}";
+  }
+
+
+
+
+
+
+  if(typeof obj === 'number'){
+    return ''+ obj;
+  }
+
+  if(typeof obj === 'boolean'){
+    if(obj){
+      return ''+ true;
+    } else {
+      return '' + false;
+    }
+  }
+
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
 };
